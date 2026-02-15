e5023dd2bf95c5982a3a479dfa67dc9ea92fbac6
wullis237 <willislionel237@gmail.com>
2026-02-15 20:30:55 +0100

    modifier du fichier cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://front.drlesimgroup.com/'],
+    'allowed_origins' => ['https://font.drlesimgroup.com'],
 
     'allowed_origins_patterns' => [],
 
